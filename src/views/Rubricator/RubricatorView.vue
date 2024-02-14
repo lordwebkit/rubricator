b1e3c0e65c1f5715eb07fd6d84088857ae38926b
Kashirskiy Nikita <lordwebkit@gmail.com>
2024-02-14 09:22:48 +0300

    Create base style for rubricator view

---
+++ src/views/Rubricator/RubricatorView.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+<template>
+  <section class="rubricator r-container">
+    <div class="rubricator__inner r-inner">
+      <h1 class="rubricator__title r-title">Рубрикатор</h1>
+    </div>
+  </section>
+</template>
+<style lang="scss" scoped></style>
